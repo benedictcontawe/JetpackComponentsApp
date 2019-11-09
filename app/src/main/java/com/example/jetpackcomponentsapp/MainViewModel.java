f541e1bd18ab622a4acc6b1a3ae71babbfcacfe2
Benedict <benedictcontawe@gmail.com>
2019-11-09 14:07:55 +0800

    clear unused imports

--- app/src/main/java/com/example/jetpackcomponentsapp/MainViewModel.java
+++ app/src/main/java/com/example/jetpackcomponentsapp/MainViewModel.java
@@ -1,7 +1,5 @@
 package com.example.jetpackcomponentsapp;
 
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class MainViewModel extends ViewModel {
