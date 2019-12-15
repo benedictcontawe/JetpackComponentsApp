7d0b9c6b29db60de7b5e735944d3f36c314f6c23
Benedict <benedictcontawe@gmail.com>
2019-12-15 16:13:07 +0800

    Room Entity Class now modified to data class, repository class modified

---
+++ app/src/main/java/com/example/jetpackcomponentsapp/repository/CustomRepository.kt
@@ -0,0 +1,26 @@
+package com.example.jetpackcomponentsapp.repository
+
+import com.example.jetpackcomponentsapp.room.CustomEntity
+
+class CustomRepository : BaseRepository {
+
+    constructor()
+
+    //region CRUD Operation
+    override fun insert(customEntity: CustomEntity) {
+
+    }
+
+    override fun update(customEntity: CustomEntity) {
+
+    }
+
+    override fun delete(customEntity: CustomEntity) {
+
+    }
+
+    override fun deleteAll() {
+
+    }
+    //endregion
+}
