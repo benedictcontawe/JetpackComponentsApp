29d84aab8f9919e394d72ad8c645d898b29db0e2
Benedict <benedictcontawe@gmail.com>
2019-12-15 16:15:05 +0800

    Room Database modified

--- app/src/main/java/com/example/jetpackcomponentsapp/room/CustomDatabase.kt
+++ app/src/main/java/com/example/jetpackcomponentsapp/room/CustomDatabase.kt
@@ -5,7 +5,10 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = arrayOf(CustomEntity::class), version = 1)
+@Database(
+        entities = [CustomEntity::class],
+        version = 1
+)
 abstract class CustomDatabase : RoomDatabase {
 
     abstract fun CustomDao() : CustomDAO
@@ -14,8 +17,8 @@ abstract class CustomDatabase : RoomDatabase {
         @Volatile private var instance : CustomDatabase? = null
         private val LOCK = Any()
 
-        operator fun invoke(context: Context) = instance?: synchronized(LOCK) {
-            instance?:buildDatabase(context)
+        operator fun invoke(context: Context) : CustomDatabase = instance ?: synchronized(LOCK) {
+            instance ?: buildDatabase(context).also { instance = it }
         }
 
         private fun buildDatabase(context: Context) = Room.databaseBuilder(context,
