92077922af0947ea213e448907ad32ecea758e51
Benedict <benedictcontawe@gmail.com>
2019-12-15 16:11:06 +0800

    Room Entity Class now modified to data class, repository class modified

--- app/src/main/java/com/example/jetpackcomponentsapp/room/CustomEntity.kt
+++ app/src/main/java/com/example/jetpackcomponentsapp/room/CustomEntity.kt
@@ -5,29 +5,16 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "custom_table")
-class CustomEntity {
+data class CustomEntity(
 
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "Id")
-    private var id : Int? = null
+    private var id : Int? = null,
 
     @ColumnInfo(name = "Name")
-    private var name : String? = null
+    private var name : String? = null,
 
     @ColumnInfo(name = "Icon")
     private var icon : Int? = null
 
-    constructor(name : String,icon : Int) {
-        this.name = name
-        this.icon = icon
-    }
-
-    fun getId() : Int? = this.id
-
-    fun getName() : String? {
-        return this.name
-    }
-
-    fun getIcon() : Int? = this.icon
-
-}
+)
