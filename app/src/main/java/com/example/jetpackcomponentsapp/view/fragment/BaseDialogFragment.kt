1fffdd100b074461bbb57d3e3cbb7b3b8666545e
Benedict <benedictcontawe@gmail.com>
2020-03-18 14:03:03 +0800

    Create BaseFragment and BaseDialogFragment

---
+++ app/src/main/java/com/example/jetpackcomponentsapp/view/fragment/BaseDialogFragment.kt
@@ -0,0 +1,8 @@
+package com.example.jetpackcomponentsapp.view.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.DialogFragment
+
+abstract class BaseDialogFragment : DialogFragment() {
+
+}
---
+++ app/src/main/java/com/example/jetpackcomponentsapp/view/fragment/BaseFragment.kt
@@ -0,0 +1,8 @@
+package com.example.jetpackcomponentsapp.view.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment : Fragment() {
+
+}
