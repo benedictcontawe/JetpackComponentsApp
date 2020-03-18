378dc19d81291802c48c761388f4ea9468547f75
Benedict <benedictcontawe@gmail.com>
2020-03-18 15:51:32 +0800

    Update DAO Class

--- app/src/main/java/com/example/jetpackcomponentsapp/room/CustomDAO.kt
+++ app/src/main/java/com/example/jetpackcomponentsapp/room/CustomDAO.kt
@@ -9,20 +9,27 @@ interface CustomDAO {
     //@Insert
     //@Insert(onConflict = OnConflictStrategy.REPLACE)
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    fun insert(customEntity: CustomEntity)
+    suspend fun insert(customEntity: CustomEntity)
 
     @Update
-    fun update(customEntity: CustomEntity)
+    suspend fun update(customEntity: CustomEntity)
 
     //@Delete
     //fun delete(customEntity: CustomEntity)
 
     @Query("DELETE FROM custom_table WHERE Id = :id")
-    fun delete(id : Int?)
+    suspend fun delete(id : Int?)
 
     @Query("DELETE FROM custom_table")
-    fun deleteAll()
+    suspend fun deleteAll()
 
-    @Query("SELECT * FROM custom_table")
+    //@Query("SELECT * FROM custom_table")
+    @Query("SELECT * FROM custom_table ORDER BY Id ASC")
     fun getAll() : LiveData<List<CustomEntity>>
+
+    @Transaction
+    suspend fun resetDAO() {
+        deleteAll()
+
+    }
 }
