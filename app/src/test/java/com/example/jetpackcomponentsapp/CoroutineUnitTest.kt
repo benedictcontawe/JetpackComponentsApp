b1af23434125cda90dd8a20787169f1c727fe42f
Benedict <benedictcontawe@gmail.com>
2020-03-18 18:02:07 +0800

    Add test unit class

---
+++ app/src/test/java/com/example/jetpackcomponentsapp/CoroutineUnitTest.kt
@@ -0,0 +1,162 @@
+package com.example.jetpackcomponentsapp
+
+import org.junit.Assert
+import org.junit.Test
+
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.coroutineScope
+import kotlin.concurrent.thread
+
+class CoroutineUnitTest {
+
+    @Test
+    public fun firstCoroutine() {
+        System.out.println("First coroutine")
+        GlobalScope.launch {
+            delay(1000) //Non-blocking Function
+            System.out.println("Current thread (${Thread.currentThread().name})")
+            System.out.println("Task 1 ")
+            System.out.println("---------------------------------------------")
+        }
+
+        System.out.println("Task 2")
+        System.out.println("Current thread (${Thread.currentThread().name})")
+        System.out.println("---------------------------------------------")
+        Thread.sleep(2000) //Blocking Function
+
+        //Assert.assertEquals(true, true)
+        assert(true)
+    }
+
+    @Test
+    public fun withThread() {
+        System.out.println("Replace coroutine with thread")
+        thread {
+            Thread.sleep(1000) //Blocking Function
+            System.out.println("Current thread (${Thread.currentThread().name})")
+            System.out.println("Task 1")
+            System.out.println("---------------------------------------------")
+        }
+
+        System.out.println("Task 2")
+        System.out.println("Current thread (${Thread.currentThread().name})")
+        System.out.println("---------------------------------------------")
+        Thread.sleep(2000) //Blocking Function
+
+        assert(true)
+    }
+
+    @Test
+    public fun removeBlockingCode() {
+        System.out.println("Remove blocking code")
+        GlobalScope.launch {
+            delay(1000) //Non-blocking Function
+            System.out.println("Current thread (${Thread.currentThread().name})")
+            System.out.println("Task 1 ")
+            System.out.println("---------------------------------------------")
+        }
+
+        runBlocking {
+            System.out.println("Task 2")
+            System.out.println("Current thread (${Thread.currentThread().name})")
+            System.out.println("---------------------------------------------")
+            delay(2000) //Non-blocking Function
+        }
+
+
+        assert(true)
+    }
+
+    @Test
+    public fun removeExplicitWaitJob() : Unit = runBlocking {
+        System.out.println("Remove explicit wait using job")
+        val fisrtJob : Job = launch {
+            delay(1000) //Non-blocking Function
+            System.out.println("Current thread (${Thread.currentThread().name})")
+            System.out.println("Task 1 ")
+            System.out.println("---------------------------------------------")
+        }
+
+        System.out.println("Task 2")
+        System.out.println("Current thread (${Thread.currentThread().name})")
+        System.out.println("---------------------------------------------")
+        fisrtJob.join()
+
+        assert(true)
+    }
+
+    @Test
+    public fun removeExplicitWaitJobSimplified() : Unit = runBlocking {
+        System.out.println("Remove explicit wait using job Simplified")
+        launch {
+            delay(1000) //Non-blocking Function
+            System.out.println("Current thread (${Thread.currentThread().name})")
+            System.out.println("Task 1 ")
+            System.out.println("---------------------------------------------")
+        }
+
+        System.out.println("Task 2")
+        System.out.println("Current thread (${Thread.currentThread().name})")
+        System.out.println("---------------------------------------------")
+
+        assert(true)
+    }
+
+    @Test
+    public fun scopeBuilder() : Unit = runBlocking {
+        System.out.println("Coroutine Scope Builder")
+
+        //runBlocking - blocks the thread
+        //coroutineScope - Non blocking thread
+
+        launch {
+            delay(100) //Non-blocking Function
+            System.out.println("Current thread (${Thread.currentThread().name})")
+            System.out.println("Inside Run Blocking Scope")
+            System.out.println("---------------------------------------------")
+        }
+
+        coroutineScope {
+            launch {
+                delay(300)
+                System.out.println("Current thread (${Thread.currentThread().name})")
+                System.out.println("Nested launch")
+                System.out.println("---------------------------------------------")
+            }
+            delay(200)
+            System.out.println("Current thread (${Thread.currentThread().name})")
+            System.out.println("Inside Coroutine Scope")
+            System.out.println("---------------------------------------------")
+        }
+
+        System.out.println("Current thread (${Thread.currentThread().name})")
+        System.out.println("Run Blocking scope over")
+        System.out.println("---------------------------------------------")
+
+        assert(true)
+    }
+
+    @Test
+    public fun loopCoroutine() : Unit = runBlocking {
+        repeat(100) {
+            delay(1)
+            println("Light weight thread")
+        }
+
+        assert(true)
+    }
+
+    @Test
+    public fun loopThread() : Unit = runBlocking {
+        repeat(100) {
+            Thread.sleep(1)
+            println("Light weight thread")
+        }
+
+        assert(true)
+    }
+}
