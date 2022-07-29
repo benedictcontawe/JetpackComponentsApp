b1e9a2add01fc2321af2f25809863ce174bb6898
Benedict Contawe <benedict.contawe@afpayments.com>
2022-07-29 17:52:27 +0800

    Convert to object

--- app/src/main/java/com/example/jetpackcomponentsapp/util/ConvertList.kt
+++ app/src/main/java/com/example/jetpackcomponentsapp/util/ConvertList.kt
@@ -7,56 +7,44 @@ import com.example.jetpackcomponentsapp.model.CustomEntity
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.*
 
-class ConvertList {
+object ConvertList {
 
-    companion object {
-        public  fun toListModel(customEntity : List<CustomEntity>) : List<CustomModel> {
-            val itemList : MutableList<CustomModel> = mutableListOf<CustomModel>()
-            customEntity.map {
-                itemList.add(
-                        CustomModel(it.id?:0, it.name?:"")
-                )
-            }
-            return itemList
-        }
-
-        public fun toLiveDataListModel(localList : LiveData<List<CustomEntity>>) : LiveData<List<CustomModel>> {
-            return Transformations.map<
-                    List<CustomEntity>, //localList Data Type
-                    List<CustomModel> //toListModel List Data Type
-                    >(
-                    localList,
-                    Companion::toListModel
+    private fun toListModel(customEntity : List<CustomEntity>) : List<CustomModel> {
+        val itemList : MutableList<CustomModel> = mutableListOf<CustomModel>()
+        customEntity.map {
+            itemList.add(
+                CustomModel(it.id?:0, it.name?:"")
             )
         }
+        return itemList
+    }
 
-        public suspend fun toStateFlowListModel(localList : Flow<List<CustomEntity>>, scope : CoroutineScope) : StateFlow<List<CustomModel>> {
-            return localList.mapLatest { entityList ->
-                toListModel(entityList)
-            }.stateIn(scope = scope/*, SharingStarted.Eagerly, initialValue = false*/)
+    fun toLiveDataListModel(localList : LiveData<List<CustomEntity>>) : LiveData<List<CustomModel>> {
+        return Transformations.map<List<CustomEntity>, List<CustomModel>>(localList) {
+            toListModel(it)
         }
+    }
 
-        public suspend fun toSharedFlowListModel(localList : Flow<List<CustomEntity>>, scope : CoroutineScope) : SharedFlow<List<CustomModel>> {
-            return localList.mapLatest { entityList ->
-                toListModel(entityList)
-            }.stateIn(scope = scope)
-        }
+    suspend fun toStateFlowListModel(localList : Flow<List<CustomEntity>>, scope : CoroutineScope) : StateFlow<List<CustomModel>> {
+        return localList.mapLatest { entityList ->
+            toListModel(entityList)
+        }.stateIn(scope = scope/*, SharingStarted.Eagerly, initialValue = false*/)
+    }
 
-        public fun toEntity(customModel: CustomModel) : CustomEntity {
-            return when(customModel.id) {
-                null -> {
-                    CustomEntity(
-                        customModel.name?:"",
-                        customModel.icon?:0
-                    )
-                }
-                else -> {
-                    CustomEntity(
-                        customModel.id!!,
-                        customModel.name?:"",
-                        customModel.icon?:0
-                    )
-                }
+    fun toEntity(customModel: CustomModel) : CustomEntity {
+        return when(customModel.id) {
+            null -> {
+                CustomEntity(
+                    customModel.name?:"",
+                    customModel.icon?:0
+                )
+            }
+            else -> {
+                CustomEntity(
+                    customModel.id!!,
+                    customModel.name?:"",
+                    customModel.icon?:0
+                )
             }
         }
     }
