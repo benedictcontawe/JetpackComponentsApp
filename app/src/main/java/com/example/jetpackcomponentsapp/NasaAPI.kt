52cfe1e241d1b6ce91e1f422bbbd042259dfdeef
Benedict Contawe <benedictcontawe@gmail.com>
2022-09-24 20:44:25 +0800

    Update some comment

--- app/src/main/java/com/example/jetpackcomponentsapp/NasaAPI.kt
+++ app/src/main/java/com/example/jetpackcomponentsapp/NasaAPI.kt
@@ -4,7 +4,7 @@ import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-interface NasaAPI { //https://api.nasa.gov?api_key=xxxxx&count=###
+interface NasaAPI { //https://api.nasa.gov/planetary/apod?api_key=xxxxx&count=###
     @GET(Constants.API_GET) //@HTTP(method = "GET", path = Constants.API_GET, hasBody = true)
     public fun getAstronomyPictureOfTheDay( //@Body model : NasaRequestModel
         @Query("api_key") key : String,
