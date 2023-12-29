47e3ab1c4c453469f7f9dae0f9a7fc8e482facda
Benedict Contawe <benedictcontawe@gmail.com>
2023-12-29 11:22:50 +0800

    update some files, and upgrade gradle

--- app/src/main/java/com/example/jetpackcompose/AppDelegate.kt
+++ app/src/main/java/com/example/jetpackcompose/AppDelegate.kt
@@ -1,13 +1,12 @@
 package com.example.jetpackcompose
 
-import android.content.Context
-import androidx.multidex.MultiDex
 import androidx.multidex.MultiDexApplication
 
 class AppDelegate : MultiDexApplication() {
-
+    /*
     override fun attachBaseContext(base : Context?) {
         super.attachBaseContext(base)
         MultiDex.install(this)
     }
+    */
 }
